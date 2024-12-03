7add210ff029222999e2a89a8f09c44201b2e02f
0furkancolak <furkanfurkan228@gmail.com>
2024-12-03 15:23:17 +0300

    Update FooNavProvider to toggle visibility based on dashboard path
    - Changed initial visibility state from false to true.
    - Updated useEffect logic to hide the component when the path includes "dashboard".

--- frontend/components/layout/provider/FooNavProvider.tsx
+++ frontend/components/layout/provider/FooNavProvider.tsx
@@ -3,16 +3,16 @@ import { usePathname } from '@/i18n/routing';
 import React, { useEffect, useState } from 'react'
 
 export default function FooNavProvider({ children }: { children: React.ReactNode }) {
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(true);
     const path = usePathname();
 
     useEffect(() => {
         if (path.split("/")[1] == "dashboard") {
-            setVisible(true);
+            setVisible(false);
         }
     }, [path]);
 
-    if (visible) {
+    if (!visible) {
         return <></>;
     }
 
